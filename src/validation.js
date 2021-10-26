fc3ab90f1c2397b0c811ec5735ae54a6c3d8aa2d
Anna W <tester.kodilla@gmail.com>
2021-10-26 17:21:00 +0100

    Refactored validation

--- src/validation.js
+++ src/validation.js
@@ -1,19 +1,14 @@
-const isValidGrid = grid => {
-    return Array.isArray(grid) && grid.length === 2 && grid.every(element => Number.isInteger(element) && element >= 0);
-};
+const isValidGrid = grid =>
+    Array.isArray(grid) && grid.length === 2 && grid.every(element => Number.isInteger(element) && element >= 0);
 
-const isValidPosition = (x, y, maxX, maxY) => {
-    return x >= 0 && x <= maxX && y >= 0 && y <= maxY;
-};
+const isValidPosition = (x, y, maxX, maxY) => x >= 0 && x <= maxX && y >= 0 && y <= maxY;
 
-const isValidPositionAndDirection = (grid, position) => {
-    return Array.isArray(position) && position.length === 3 && Number.isInteger(position[0]) && Number.isInteger(position[1]) &&
-        /^[NSWE]{1}$/g.test(position[2]) && isValidPosition(position[0], position[1], ...grid);
-};
+const isValidPositionAndDirection = (grid, position) =>
+    Array.isArray(position) && position.length === 3 && position.slice(0, 2).every(Number.isInteger) &&
+    /^[NSWE]{1}$/g.test(position[2]) && isValidPosition(position[0], position[1], ...grid);
 
-const isValidOccupied = (grid, occupied) => {
-    return (Array.isArray(occupied) && occupied.every(element => Array.isArray(element) && isValidPositionAndDirection(grid, element)));
-}
+const isValidOccupied = (grid, occupied) =>
+    Array.isArray(occupied) && occupied.every(element => Array.isArray(element) && isValidPositionAndDirection(grid, element));
 
 module.exports = {
     isValidGrid,
