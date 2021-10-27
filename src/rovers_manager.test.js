ff17af13514c350399113f02fce474b2f46256e7
Anna W <tester.kodilla@gmail.com>
2021-10-27 20:25:13 +0100

    Refactored tests for easier maintenance

--- src/rovers_manager.test.js
+++ src/rovers_manager.test.js
@@ -1,24 +1,22 @@
 const manageRovers = require("./rovers_manager");
 
 describe("manageRovers", () => {
-    test("returns an array of new positions when all arguments valid and no clashes", () => {
-        expect(manageRovers([3, 3], [[1, 2, "N"]], ["M"])).toEqual([[1, 3, "N"]]);
-        expect(manageRovers([7, 4], [[1, 2, "N"], [2, 2, "N"]], ["LM", "RM"])).toEqual([[0, 2, "W"], [3, 2, "E"]]);
-        expect(manageRovers([5, 5], [[1, 2, "N"], [3, 3, "E"]], ["LMLMLMLMM", "MMRMMRMRRM"])).toEqual([[1, 3, "N"], [5, 1, "E"]]);
-        expect(manageRovers([15, 5], [[11, 3, "W"]], ["MLLMMRMM"])).toEqual([[12, 1, "S"]]);
-        const result = manageRovers([10, 10], [[0, 0, "N"], [3, 3, "E"], [7, 5, "W"]], ["RMLM", "MMRM", "LLMMM"]);
-        const expected = [[1, 1, "N"], [5, 2, "S"], [10, 5, "E"]];
-        expect(result).toEqual(expected);
+    test.each([
+        { grid: [3, 3], positions: [[1, 2, "N"]], instructions: ["M"], expected: [[1, 3, "N"]] },
+        { grid: [7, 4], positions: [[1, 2, "N"], [2, 2, "N"]], instructions: ["LM", "RM"], expected: [[0, 2, "W"], [3, 2, "E"]] },
+        { grid: [5, 5], positions: [[1, 2, "N"], [3, 3, "E"]], instructions: ["LMLMLMLMM", "MMRMMRMRRM"], expected: [[1, 3, "N"], [5, 1, "E"]] },
+        { grid: [15, 5], positions: [[11, 3, "W"]], instructions: ["MLLMMRMM"], expected: [[12, 1, "S"]] },
+        { grid: [10, 10], positions: [[0, 0, "N"], [3, 3, "E"], [7, 5, "W"]], instructions: ["RMLM", "MMRM", "LLMMM"], expected: [[1, 1, "N"], [5, 2, "S"], [10, 5, "E"]] },
+    ])("returns an array of new positions when all arguments valid and no clashes", ({ grid, positions, instructions, expected }) => {
+        expect(manageRovers(grid, positions, instructions)).toEqual(expected);
     });
 
-    test("returns an array of last possible positions when all arguments valid but rovers clashing", () => {
-        expect(manageRovers([7, 4], [[1, 2, "E"], [2, 2, "W"]], ["M", "RM"])).toEqual([[1, 2, "E"], [2, 3, "N"]]);
-        expect(manageRovers([5, 5], [[3, 2, "N"], [3, 3, "E"]], ["LMRMRMLMM", "MMRMMRMRRM"])).toEqual([[2, 3, "E"], [5, 1, "E"]]);
-        const result = manageRovers([10, 10], [[0, 0, "N"], [3, 3, "W"], [4, 5, "W"]], ["RMLM", "MMLMMR", "LMMMRMMM"]);
-        const expected = [[1, 1, "N"], [1, 2, "S"], [2, 2, "W"]];
-        expect(result).toEqual(expected);
-        const result2 = manageRovers([10, 10], [[0, 0, "N"], [3, 3, "W"], [4, 3, "W"]], ["RMLM", "MRMRMRM", "M"]);
-        const expected2 = [[1, 1, "N"], [3, 3, "S"], [4, 3, "W"]];
-        expect(result2).toEqual(expected2);
+    test.each([
+        { grid: [7, 4], positions: [[1, 2, "E"], [2, 2, "W"]], instructions: ["M", "RM"], expected: [[1, 2, "E"], [2, 3, "N"]] },
+        { grid: [5, 5], positions: [[3, 2, "N"], [3, 3, "E"]], instructions: ["LMRMRMLMM", "MMRMMRMRRM"], expected: [[2, 3, "E"], [5, 1, "E"]] },
+        { grid: [10, 10], positions: [[0, 0, "N"], [3, 3, "W"], [4, 5, "W"]], instructions: ["RMLM", "MMLMMR", "LMMMRMMM"], expected: [[1, 1, "N"], [1, 2, "S"], [2, 2, "W"]] },
+        { grid: [10, 10], positions: [[0, 0, "N"], [3, 3, "W"], [4, 3, "W"]], instructions: ["RMLM", "MRMRMRM", "M"], expected: [[1, 1, "N"], [3, 3, "S"], [4, 3, "W"]] },
+    ])("returns an array of last possible positions when all arguments valid but rovers clashing", ({ grid, positions, instructions, expected }) => {
+        expect(manageRovers(grid, positions, instructions)).toEqual(expected);
     });
 });
